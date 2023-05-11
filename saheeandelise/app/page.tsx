9c877fe7694da8bf6cec09b1c0dbad1d0c359400
sahee <saheethao@yahoo.com>
2023-05-10 23:29:33 -0500

    fix build

--- saheeandelise/app/page.tsx
+++ saheeandelise/app/page.tsx
@@ -1,10 +1,9 @@
 // pages/index.js
 import React from 'react';
-//import Link from 'next/link';
 
 export default function Page() { return (
   <div>
-    <h1>Welcome to Sahee and Elise's website!</h1>
+    <h1>Welcome to Sahee and Elise&apos;s website!</h1>
   </div>
 )
 };
