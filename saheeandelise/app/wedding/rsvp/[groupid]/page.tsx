b0b53f8fd7d1d3f4346ec9ef178f19173b1e4ec0
elisebaumgartner <elise.baumgartner97@gmail.com>
2024-03-01 14:31:16 -0600

    fix rsvp

--- saheeandelise/app/wedding/rsvp/[groupid]/page.tsx
+++ saheeandelise/app/wedding/rsvp/[groupid]/page.tsx
@@ -6,10 +6,16 @@ import { RsvpForm } from '../../models/RsvpForm';
 import TablePreferences from '../../components/TableSelect';
 import Link from 'next/link';
 
-export default function Page() {
+export default function Page({
+  params
+}: {
+  params: { groupid: string }
+}) {
+  //const searchParams = useSearchParams()
+  //const groupId = searchParams.get('groupid')
+  const groupId = params.groupid;
 
-  const searchParams = useSearchParams()
-  const groupId = searchParams.get('groupid')
+
   if (!groupId) {
     redirect('/wedding/rsvp')
   }
@@ -27,7 +33,7 @@ export default function Page() {
   //use this only for debugging
   const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
-  const router = useRouter()
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -132,7 +138,7 @@ export default function Page() {
 
       {/* Loading Icon */}
       <div className={`w-full flex flex-col justify-center items-center ${isLoading ? 'visible mt-20' : 'hidden'}`} role="status">
-        <svg aria-hidden="true" className="inline w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-weddingMaroon" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg aria-hidden="true" className="inline w-20 h-20 text-gray-200 animate-spin fill-weddingMaroon" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
         </svg>
@@ -151,7 +157,7 @@ export default function Page() {
                 <ul className="grid w-full gap-6 md:grid-cols-2 mb-2">
                   <li>
                     <input id={"attending-yes-" + person.id} className="hidden peer" {...register(`people.${index}.willAttend`)} type="radio" value="true" defaultChecked={person.willAttend === 'true'} />
-                    <label htmlFor={"attending-yes-" + person.id} className="inline-flex items-center justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <label htmlFor={"attending-yes-" + person.id} className="inline-flex items-center justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100">
                       <div className="block">
                         <div className="w-full font-semibold">Yes</div>
                       </div>
@@ -159,7 +165,7 @@ export default function Page() {
                   </li>
                   <li>
                     <input id={"attending-no-" + person.id} className="hidden peer" {...register(`people.${index}.willAttend`)} type="radio" value="false" defaultChecked={person.willAttend === 'false'} />
-                    <label htmlFor={"attending-no-" + person.id} className="inline-flex items-center justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <label htmlFor={"attending-no-" + person.id} className="inline-flex items-center justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100">
                       <div className="block">
                         <div className="w-full font-semibold">No</div>
                       </div>
@@ -173,7 +179,7 @@ export default function Page() {
                     <ul className="grid w-full gap-6 md:grid-cols-3">
                       <li>
                         <input id={"meal-adult-" + person.id} className="hidden peer" {...register(`people.${index}.mealPreference`, { required: true })} type="radio" value="adult" defaultChecked={person.mealPreference === 'adult'} />
-                        <label htmlFor={"meal-adult-" + person.id} className="h-full inline-flex justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <label htmlFor={"meal-adult-" + person.id} className="h-full inline-flex justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100">
                           <div className="block">
                             <div className="w-full font-semibold">Adult</div>
                             <div className="w-full">Buffet-styled</div>
@@ -182,7 +188,7 @@ export default function Page() {
                       </li>
                       <li>
                         <input id={"meal-child-" + person.id} className="hidden peer" {...register(`people.${index}.mealPreference`, { required: true })} type="radio" value="child" defaultChecked={person.mealPreference === 'child'} />
-                        <label htmlFor={"meal-child-" + person.id} className="inline-flex items-center justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <label htmlFor={"meal-child-" + person.id} className="inline-flex items-center justify-between w-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100">
                           <div className="block">
                             <div className="w-full font-semibold">Child</div>
                             <div className="w-full">Includes chicken nuggets, mac and cheese, and applesauce</div>
@@ -191,7 +197,7 @@ export default function Page() {
                       </li>
                       <li>
                         <input id={"meal-none-" + person.id} className="hidden peer" {...register(`people.${index}.mealPreference`, { required: true })} type="radio" value="none" defaultChecked={person.mealPreference === 'none'} />
-                        <label htmlFor={"meal-none-" + person.id} className="inline-flex justify-between w-full h-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                        <label htmlFor={"meal-none-" + person.id} className="inline-flex justify-between w-full h-full p-2 text-gray-500 border border-gray-200 rounded-lg cursor-pointer peer-checked:border-weddingMaroon peer-checked:text-weddingMaroon hover:text-gray-600 hover:bg-gray-100">
                           <div className="block">
                             <div className="w-full font-semibold">No meal</div>
                             <div className="w-full">Ex: This guest is a baby</div>
@@ -303,7 +309,7 @@ export default function Page() {
             {/* Loading Icon */}
             {/* className={`flex flex-col justify-center items-center ${isLoading || hasError ? 'hidden' : 'visible'}`} */}
             <div className={`w-full flex flex-col justify-center items-center ${isSubmitLoading ? 'visible' : 'hidden'}`} role="status">
-              <svg aria-hidden="true" className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-weddingMaroon" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg aria-hidden="true" className="inline w-10 h-10 text-gray-200 animate-spin fill-weddingMaroon" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
               </svg>
--- saheeandelise/next.config.js
+++ saheeandelise/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: false,
+    trailingSlash: true,
     output: "export",
     /*If your next.config.js file includes { output: 'export' }, this configuration is incompatible 
     with the Image Optimization API provided by Next.js.  */
