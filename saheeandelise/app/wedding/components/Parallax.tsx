30362ba8d332bde9fedcca0186bf47e98a3d9879
sahee <saheethao@yahoo.com>
2024-03-10 19:39:06 -0500

    home changes

--- saheeandelise/app/wedding/components/Parallax.tsx
+++ saheeandelise/app/wedding/components/Parallax.tsx
@@ -1,17 +1,43 @@
 import Link from "next/link";
-import { ParallaxBanner } from "react-scroll-parallax";
+import { ParallaxBanner, ParallaxBannerLayer } from "react-scroll-parallax";
 
 const MainParallax = () => {
   return (
     <ParallaxBanner
-      layers={[
-        { image: '/images/woods.png', speed: -20 },
-        // { image: '/images/vine.png', speed: -10 },
-      ]}
-      className="aspect-[1/1] md:aspect-[2/1]"
-    >
+      className="aspect-[2/1] md:aspect-[2/1]">
+      <ParallaxBannerLayer
+        speed={-10}
+        className="bg-top"
+      >
+        <img src="/images/woods-0.png" className="object-top" />
+      </ParallaxBannerLayer>
+      <ParallaxBannerLayer
+        speed={-3}
+        className="bg-top"
+      >
+        <img src="/images/woods-1.png" className="object-top" />
+      </ParallaxBannerLayer>
+      <ParallaxBannerLayer
+        speed={-3}
+        className="bg-top"
+      >
+        <img src="/images/woods-2.png" className="object-top" />
+      </ParallaxBannerLayer>
+      <ParallaxBannerLayer
+        speed={10}
+        className="bg-top"
+      >
+        <img src="/images/woods-3.png" className="object-top" />
+      </ParallaxBannerLayer>
+      <ParallaxBannerLayer
+        speed={10}
+        className="bg-top"
+      >
+        <img src="/images/woods-3.png" className="object-top" />
+      </ParallaxBannerLayer>
+
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="p-5 md:text-xl bg-white m-4 md:m-0 md:w-1/2 rounded-lg text-center">
+        <div className="p-5 md:text-xl bg-white/[.6] m-4 md:m-0 md:w-1/2 rounded-lg text-center">
           Greetings, dear traveler, to the world of our wedding website.
           We&apos;ve crafted this realm as a gateway into our story, filled with delightful details
           and fun facts for you to discover and enjoy. Within these pages, you&apos;ll find
